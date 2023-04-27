e0965ceac7ec24b7f6b9d386beb27e7f9c7090d3
Lordrem <122307445+Lordrem@users.noreply.github.com>
2023-04-27 12:17:43 -0400

    Add files via upload

---
+++ football_function.R
@@ -0,0 +1,19 @@
+#' This function allows you to express your love for football.
+#'
+#' @param love Do you love football? Defaults to TRUE.
+#'
+#' @return TRUE
+#' @export
+#'
+#' @examples football_function()
+football_function <- function(love=TRUE) {
+  if(love==TRUE){
+    print("I love football!")
+  }
+  else{
+    print("Football is the best sport.")
+  }
+}
+
+
+
